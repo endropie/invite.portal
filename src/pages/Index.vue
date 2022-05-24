d1fd3383a0b47bbae1f715c833b46d0718db6201
endropie <hendrowibo@msn.com>
2022-05-25 01:41:11 +0700

    initial commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,18 @@
+<template>
+  <q-page class="flex flex-center">
+    <q-img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-vertical.svg"
+      style="width: 200px; height: 200px"
+    />
+  </q-page>
+</template>
+
+<script>
+import { api } from "src/boot/axios";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "PageIndex",
+});
+</script>
